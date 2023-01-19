f2ba0f88333caf7797242465da254c20dfde8cf8
Nasir Mustapha <teboy.com@gmail.com>
2023-01-19 15:21:16 +0100

    objects and recap

---
+++ objects.js
@@ -0,0 +1,34 @@
+person = {name:"Zaiyad", age:25, friend:"Khadija"}
+
+console.log(person.name)
+console.log(person.age)
+console.log(person.friend)
+console.log(person.toString())
+person.name = "Aisha"
+
+console.log(person.name)
+
+person2 = {
+    "name":"Sam", 
+    "age":10, 
+    "height":5.7,
+    "hobbies":[
+        "Swimming", 
+        "Football"
+    ],
+    "isFemale":false,
+    "friends":[
+        {"name":"Steve", "age":16},
+        {"name":"Ibrahim", "age":5}
+    ]
+}
+
+console.log(person2)
+
+console.log("Hello, my name is ".concat(person2.name, " I am ", person2.age, " and I like ", person2.hobbies.at(0)))
+
+console.log("Hello, my name is ".concat(person2.friends.at(0).name))
+
+console.log(person2.friends.at(1).name)
+
+console.log(person2.hobbies.at(1).at(0))
---
+++ recap.js
@@ -0,0 +1,36 @@
+// 757623
+// 1.344
+// ""
+// ''
+// ["coke", "cake",["ibrahim",["zayad",{"name":"sam","age":30, "height":5.3
+// }]]]
+// true
+// false
+// preson = ["758732"]
+
+
+// &&
+// ||
+console.log(true||false)
+console.log(true&&false)
+
+// includes
+// length
+// indexOf
+// endsWith
+// lastIndexOf
+
+
+person = "Ziyad"
+
+console.log(person.includes('f'))
+console.log(person.length)
+console.log(person.indexOf("a"))
+
+person = ["biscuit","tea", "oranges", "apple"]
+console.log(person.at(0))
+console.log(person.flat(2))
+console.log(person.slice(0, 4))
+console.log(person.sort())
+
+
