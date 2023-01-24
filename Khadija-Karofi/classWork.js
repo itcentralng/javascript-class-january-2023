657e346fc640d2ccbaab2b53f9738ed1e24c3c59
umeetarh <khadeejertouhkarofi@gmail.com>
2023-01-24 13:47:10 +0100

    assignment and classwork

---
+++ Khadija-Karofi/classWork.js
@@ -0,0 +1,10 @@
+shoplist = ["mom", "brothers", "sister"]
+
+if (shoplist.includes("coke")) {
+    console.log(shoplist)
+} else{
+    console.log(shoplist.push("coke"))
+    console.log(shoplist)
+}
+  
+
