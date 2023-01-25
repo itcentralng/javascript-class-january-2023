16beb5f1f88a559a90d499315afd71f2407ef9e3
Nasir Mustapha <teboy.com@gmail.com>
2023-01-25 13:56:14 +0100

    move zayad's assignment to his folder

--- conditionals.js
+++ conditionals.js
@@ -60,3 +60,42 @@
 // Use conditionals to check if "Coke" is in your list
 // If it is not, add it and then print the list
 // Note that your array can not have "Coke" when initialized
+
+// shop = ["Cake"]
+
+// if (shop.indexOf("Coke") == -1){
+//     shop.push("Coke")
+//     console.log(shop)
+// }
+// else if (shop.includes('Cake')){
+//     console.log("but shop has cake")
+// }
+// else if (shop.includes('Cake')){
+//     console.log("but shop has cake")
+// }
+
+if (1>0){
+    console.log("1 is greater")
+}
+else if (2>3){
+    console.log("2 is greater")
+}
+else if (3>4){
+    console.log("3 is greater")
+}
+else{
+    console.log("None of the conditions are met")
+}
+
+if (true || (false && true)){
+    console.log("All the conditons are true")
+}
+
+// Quick Exercise
+
+// Create an array of 5 numbers 1,2,3,4,5
+// Check if the last number is greater than
+// 12 and print out the number
+// else check if the first number is greater
+// than zero and print out the number
+// else print the entire array
