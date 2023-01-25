748d65f810b16336484e193929f1f820c92b1fa7
Ziyad <ahmedzeeyad02@gmail.com>
2023-01-25 13:51:31 +0100

    commit conditionalsZee

---
+++ ConditionalsZee.js
@@ -0,0 +1,24 @@
+sprite = [1,2,3,4,5]
+
+if(sprite.at(4)>12){
+    console.log(sprite.at(4));
+
+}
+ else if (sprite.at(0)>0){
+ console.log(sprite.at(0))
+
+} 
+else {
+    (console.log(sprite));
+
+}
+console.log(sprite.at(0))
+
+
+
+
+
+
+
+  
+
