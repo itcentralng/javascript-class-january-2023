2e5488cd6133205055eb04beab89eaa143394de5
umeetarh <khadeejertouhkarofi@gmail.com>
2023-01-25 13:53:58 +0100

    initial classwork commit+

--- Khadija-Karofi/classWork.js
+++ Khadija-Karofi/classWork.js
@@ -1,10 +1,18 @@
-shoplist = ["mom", "brothers", "sister"]
+/*shoplist = ["mom", "brothers", "sister"]
 
 if (shoplist.includes("coke")) {
     console.log(shoplist)
 } else{
     console.log(shoplist.push("coke"))
     console.log(shoplist)
+}*/
+  num = [1,2,3,4,5]
+
+if (num.at(4) > 12) {
+    console.log(num.at(4)) 
+} else if (num.at(0) > 0) {
+    console.log(num.at(0))
+} else {
+    console.log(num)
 }
-  
 
