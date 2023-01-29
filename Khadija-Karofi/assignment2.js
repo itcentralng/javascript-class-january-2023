4efa03ca582daabf734b0d4f955171ff8fe2f219
umeetarh <khadeejertouhkarofi@gmail.com>
2023-01-29 22:05:28 +0100

    commit classWork

---
+++ Khadija-Karofi/assignment2.js
@@ -0,0 +1,51 @@
+/*str = "HELLO WORLD";
+
+/*8console.log(str.toLowerCase()
+    .split(",")
+    .reverse("")
+    .join(""));*/
+     
+/*reversestr = "hello world"
+
+if (str === reversestr) {
+    console.log("the string is a palindrome")
+} else {
+    console.log("the string is not palindrome")
+}*/
+
+/* = [1, 2, 3, 4, 5]
+i in range(len(nums))
+  nums[i] = nums[i] * 2
+console.log(nums)*/
+
+person = {
+   " name": "abba",
+    "age": "35",
+   " gender":"male",
+}
+if (person.age > 30) {
+    console.log("name: ". person.concat("name"));
+    console.log("age:" .concat("age") .person);
+    console.log("gender:"  .concat("gender").person)
+}
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
