47387e38c3c79b148f4f178fa0e37c1a2fba73b0
Nasir Mustapha <teboy.com@gmail.com>
2023-01-31 13:50:24 +0100

    more loops

--- loops.js
+++ loops.js
@@ -44,3 +44,39 @@
 // from a certain shop. The shop has a list of items each 
 // with its name, price and brand. The customer only has
 // 50 naira to spend and is looking to buy up to 5 products.
+
+shop = [
+    {name:"Coke", price:100, brand:"Coca Cola"},
+    {name:"Cake", price:15, brand:"Kanti Plus"},
+    {name:"Pepsi", price:10, brand:"Pepsi Cola"},
+    {name:"Sprite", price:140, brand:"Coca Cola"},
+    {name:"Corn", price:10, brand:"Aymans Pantry"},
+    {name:"Corn Flakes", price:100, brand:"Kellogs"},
+    {name:"Butter", price:10, brand:"Blueband"},
+]
+
+money = 50;
+
+cart_items = []
+cart_prices = []
+
+for (i=0; i<shop.length; i+=1){
+    if (shop.at(i).price <= money){
+        cart_items.push(shop.at(i).name)
+        cart_prices.push(shop.at(i).price)
+        money -= shop.at(i).price
+    }
+}
+
+thanks = "Thank you for shopping with us\n"
+items = ""
+for (i=0; i<cart_items.length; i+=1){
+    items+="\n"+cart_items.at(i)+" --- "+cart_prices.at(i);
+}
+items += "\nTOTAL:"+(50-money)
+receipt = thanks+"Here are your items:\n"+items
+
+console.log(receipt);
+if (money > 0){
+    console.log("Your change is "+money+" naira");
+}
