13c569757e29eb0a833c553047d46ffdce9f0f45
umeetarh <khadeejertouhkarofi@gmail.com>
2023-01-31 13:54:10 +0100

    classexecise.j

---
+++ Khadija-Karofi/classexecise.js
@@ -0,0 +1,29 @@
+shoplist = [
+    {
+        name: "coke",
+        brand: "cokacola",
+        price: "5n"
+    },
+    {
+        name: "foxs",
+        brand: "cookies",
+        price: "10n"
+    },
+    {
+        name: "ice-cream",
+        brand: "smartkids",
+        price:"10n"
+    },
+    {
+        name: "shawarma",
+        brand: "shawarma corner",
+        price:"15n"
+    },
+    {
+        name: "bag",
+        brand: "lv",
+        price:"10n"
+    }
+    
+]
+money = 50;
--- assignment3.js
+++ assignment3.js
@@ -4,5 +4,5 @@
 
 // Q2:
 // Write a program that loops through all
-// alphabets in the english languge and 
+// alphabets in the english languge and
 // outputs the vowels
